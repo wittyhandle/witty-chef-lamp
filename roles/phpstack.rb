c3cb00ff08542ed876d5fa0c6ba099b440ef0990
Mike Ottinger <mikeottinger@gmail.com>
2012-06-11 22:51:58 -0700

    use the mysql recipe

--- roles/phpstack.rb
+++ roles/phpstack.rb
@@ -1,4 +1,4 @@
 name "phpstack"
 description "Nodes that run php with apache and mysql"
-run_list %w(ntp apache2 apache2::mod_php5 vhost)
+run_list %w(vim ntp apache2 apache2::mod_php5 vhost mysql::server)
 override_attributes "apache" => { "listen_ports" => [80] }
