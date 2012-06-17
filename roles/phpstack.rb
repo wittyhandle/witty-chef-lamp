aa00c23079712f9add6aea58d534b1c721d1df7d
Mike Ottinger <mikeottinger@gmail.com>
2012-06-16 22:39:13 -0700

    added back a test comment

--- roles/phpstack.rb
+++ roles/phpstack.rb
@@ -2,3 +2,5 @@ name "phpstack"
 description "Nodes that run php with apache and mysql"
 run_list %w(vim ntp apache2 apache2::mod_php5 vhost mysql::server phpmyadmin)
 override_attributes "apache" => { "listen_ports" => [80] }
+
+# will it work?
