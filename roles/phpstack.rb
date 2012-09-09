8cb63297ec797d5bf30eeb933e9b03a9d8afeeae
Mike Ottinger <mikeottinger@gmail.com>
2012-09-09 08:35:44 -0700

    add the php cookbook and use its curl module

--- roles/phpstack.rb
+++ roles/phpstack.rb
@@ -1,4 +1,4 @@
 name "phpstack"
 description "Nodes that run php with apache and mysql"
-run_list %w(apt vim ntp apache2 apache2::mod_php5 mysql::server phpmyadmin client)
+run_list %w(apt vim ntp apache2 apache2::mod_php5 php::module_curl mysql::server phpmyadmin client)
 override_attributes "apache" => { "listen_ports" => [80] }
